f324423eb577960e4c59623df1e7f120460eefff
Pratiksha0401 <pratiksha411998@gmail.com>
2021-08-27 14:24:48 +0530

    Soap Producer

---
+++ src/test/java/com/countries/CountriesWsdlProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.countries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesWsdlProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
